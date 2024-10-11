8b36f45cbf18bff62142a0e7c09964d58fa2aac0
Manjesh Reddy <41574929+reddymanjesh@users.noreply.github.com>
2024-10-11 18:39:45 +0530

    Create addition.bash

---
+++ addition.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+add_numbers() {
+    local num1=$1
+    local num2=$2
+    echo $((num1 + num2))
+}
+read -p "Please Enter the first number: " number1
+read -p "Please Enter the second number: " number2
+
+result=$(add_numbers "$number1" "$number2")
+echo "The sum of $number1 and $number2 is: $result"
